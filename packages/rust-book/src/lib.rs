0d9d2c26670faa2ee99fb3610ffe25ae07f9315b
Simon Bourne <simonbourne@gmail.com>
2023-11-30 22:07:07 +0000

    Don't remove output dir

--- packages/rust-book/src/lib.rs
+++ packages/rust-book/src/lib.rs
@@ -72,7 +72,8 @@ impl Book {
     }
 
     fn build(&self) -> Result<()> {
-        fs::remove_dir_all(&self.out_dir)?;
+        // TODO: Figure out why build fails sometimes when we remove the output dir:
+        // fs::remove_dir_all(&self.out_dir)?;
         fs::create_dir_all(&self.out_dir)?;
         fs::copy(
             self.cargo_manifest_dir.join("book.toml"),
